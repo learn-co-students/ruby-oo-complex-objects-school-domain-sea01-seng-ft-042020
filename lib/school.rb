e10653e045ec534df9c5cba4c016e3bdf45d50ad
sap2411 <steven.parsons.us@gmail.com>
2020-04-21 23:20:15 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require "pry"
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @sorted = {}
+        @roster.map {|key, value| @sorted[key] = value.sort}
+        @sorted
+    end
+end
+
