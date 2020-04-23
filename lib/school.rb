515eab511556fb1b7fcfd11f5c5373d9502269b8
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-22 20:15:03 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School 
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name,grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do | grades , names |
+            # binding.pry
+            names.sort!
+        end
+    end
+end
+School.new("Name").sort
